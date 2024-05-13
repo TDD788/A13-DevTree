c409b42deca701fb2a82549ac44afae72fae95ed
TheDarkDeath788 <103203291+TDD788@users.noreply.github.com>
2024-05-13 14:38:41 -0300

    Update bootimg.mk

--- prebuilt/avb/bootimg.mk
+++ prebuilt/avb/bootimg.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-MKBOOTIMG := device/samsung/a12s/prebuilt/avb/mkbootimg
+MKBOOTIMG := device/samsung/a13/prebuilt/avb/mkbootimg
 
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
